fb66a348fe51a4736247621a1904492859fe932c
SmileyJoe <codys@roam.africa>
2019-02-04 10:20:05 +0200

    Initial commit, empty project

---
+++ app/src/main/java/smileyjoe/io/androidintro/MainActivity.java
@@ -0,0 +1,13 @@
+package smileyjoe.io.androidintro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
